af61c9b8a09a1ecebfb88daf28cf52ef4124394d
lolinone <lolinone@users.noreply.github.com>
2025-01-08 15:12:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMembers() {
+  return [
+    {
+      name: "Dylan",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Git"],
+    },
+    {
+      name: "Jenny",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Git"],
+    },
+    {
+      name: "Pete",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Git"],
+    },
+    {
+      name: "Sue",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Git"],
+    },
+    {
+      name: "Tyrell",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Git"],
+    },
+  ];
+}
